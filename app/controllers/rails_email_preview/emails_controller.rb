97b83eba2ea1543bd0d9e2f420132611421fcd93
Gleb Mazovetskiy <glex.spb@gmail.com>
2012-01-30 18:50:11 +0100

    Version 0.0.2; bugfixes

--- app/controllers/rails_email_preview/emails_controller.rb
+++ app/controllers/rails_email_preview/emails_controller.rb
@@ -12,8 +12,7 @@ class RailsEmailPreview::EmailsController < RailsEmailPreview::ApplicationContro
     @mail = @preview_class.new.send(params[:mail_action])
     body_part = @mail
     if @mail.multipart?
-      content_type = Rack::Mime.mime_type(params[:format])
-      body_part = @mail.parts.find { |part| part.content_type.match(content_type) } || @mail.parts.first
+      body_part = @mail.parts.find { |part| part.content_type.start_with?(@part_type) } || @mail.parts.first
     end
     body = body_part.body
     if body_part.content_type =~ /plain/
@@ -25,5 +24,6 @@ class RailsEmailPreview::EmailsController < RailsEmailPreview::ApplicationContro
   private
   def load_preview_class
     @preview_class = (RailsEmailPreview.preview_classes || []).find {|pc| pc.name.underscore == params[:mail_class]}
+    @part_type = params[:part_type] || 'text/html'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 RailsEmailPreview::Engine.routes.draw do
   root :to => 'emails#index'
-  match 'raw/:mail_class/:mail_action(.:format)',
+  match 'raw/:mail_class/:mail_action',
         :as => :raw_email,
         :to => 'emails#show_raw',
-        :defaults => {:format => 'html'},
         :mail_class => %r([\w/]+)
-  match ':mail_class/:mail_action(.:format)',
+  match ':mail_class/:mail_action',
         :as => :email,
         :to => 'emails#show',
-        :defaults => {:format => 'html'},
         :mail_class => %r([\w/]+)
 end
