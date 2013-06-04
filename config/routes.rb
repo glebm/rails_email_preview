302cc736c414ef288570b8e436101debb1fb81e1
gleb <glex.spb@gmail.com>
2013-06-04 23:57:33 +0200

    routes match => get

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 RailsEmailPreview::Engine.routes.draw do
   root :to => 'emails#index'
-  match 'raw/:mail_class/:mail_action',
+  get 'raw/:mail_class/:mail_action',
         :as => :raw_email,
         :to => 'emails#show_raw',
         :mail_class => %r([\w/]+)
-  match ':mail_class/:mail_action',
+  get ':mail_class/:mail_action',
         :as => :email,
         :to => 'emails#show',
         :mail_class => %r([\w/]+)
