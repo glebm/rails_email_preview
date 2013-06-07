0d2df688f1b8262a36b27535d80d64d60775e9e0
gleb <glex.spb@gmail.com>
2013-06-07 03:27:52 +0200

    refactor and keep email_locale around

--- app/controllers/rails_email_preview/emails_controller.rb
+++ app/controllers/rails_email_preview/emails_controller.rb
@@ -1,7 +1,7 @@
 class RailsEmailPreview::EmailsController < RailsEmailPreview::ApplicationController
   include ERB::Util
   before_filter :load_preview_class, except: :index
-  around_filter :with_email_locale, only: [:show, :show_raw]
+  before_filter :set_email_preview_locale
 
   # list screen
   def index
@@ -10,35 +10,39 @@ class RailsEmailPreview::EmailsController < RailsEmailPreview::ApplicationContro
 
   # preview screen
   def show
-    @part_type = params[:part_type] || 'text/html'
-    @mail      = @preview_class.new.send(params[:mail_action])
+    I18n.with_locale @email_locale do
+      @part_type = params[:part_type] || 'text/html'
+      @mail      = @preview_class.new.send(params[:mail_action])
+    end
+    render
   end
 
   # render actual email content
   def show_raw
-    @mail = @preview_class.new.send(params[:mail_action])
-    RailsEmailPreview.run_before_render(@mail)
-    if @part_type == 'raw'
-      body = "<pre id='raw_message'>#{html_escape(@mail.to_s)}</pre>"
-    else
-      if @mail.multipart?
-        body_part = (@part_type =~ /html/ ? @mail.html_part : @mail.text_part)
+    I18n.with_locale @email_locale do
+      @mail = @preview_class.new.send(params[:mail_action])
+      RailsEmailPreview.run_before_render(@mail)
+      if @part_type == 'raw'
+        body = "<pre id='raw_message'>#{html_escape(@mail.to_s)}</pre>"
       else
-        body_part = @mail
+        if @mail.multipart?
+          body_part = (@part_type =~ /html/ ? @mail.html_part : @mail.text_part)
+        else
+          body_part = @mail
+        end
+        body = body_part.body
+        if body_part.content_type =~ /plain/
+          body = "<pre id='message_body'>#{body}</body>"
+        end
       end
-      body = body_part.body
-      if body_part.content_type =~ /plain/
-        body = "<pre id='message_body'>#{body}</body>"
-      end
+      render text: body, layout: false
     end
-    render text: body, layout: false
   end
 
   protected
 
-  def with_email_locale(&block)
+  def set_email_preview_locale
     @email_locale = (params[:email_locale] || I18n.locale).to_s
-    I18n.with_locale @email_locale, &block
   end
 
   private
