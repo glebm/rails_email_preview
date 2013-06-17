6fe78b6c3d94477f9c5794fa5e05c5eb9f700242
gleb <glex.spb@gmail.com>
2013-06-17 05:31:57 +0200

    Fix minor bug in MainAppRouteDelegator

--- lib/rails_email_preview/main_app_route_delegator.rb
+++ lib/rails_email_preview/main_app_route_delegator.rb
@@ -1,7 +1,7 @@
 module RailsEmailPreview::MainAppRouteDelegator
   # delegate url helpers to main_app
   def method_missing(method, *args, &block)
-    if method.to_s =~ /_(?:path|url)$/ && main_app.respond_to?(method)
+    if main_app_route_method?(method)
       main_app.send(method, *args)
     else
       super
@@ -9,6 +9,11 @@ module RailsEmailPreview::MainAppRouteDelegator
   end
 
   def respond_to?(method)
-    super || main_app.respond_to?(method)
+    super || main_app_route_method?(method)
+  end
+
+  private
+  def main_app_route_method?(method)
+    method.to_s =~ /_(?:path|url)$/ && main_app.respond_to?(method)
   end
 end
