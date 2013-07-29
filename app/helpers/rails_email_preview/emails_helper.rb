feb5b8b31303082b460d361e05a1330cd1120383
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-07-29 20:37:52 +0200

    list style fix

--- app/helpers/rails_email_preview/emails_helper.rb
+++ app/helpers/rails_email_preview/emails_helper.rb
@@ -6,4 +6,22 @@ module RailsEmailPreview::EmailsHelper
       value.to_s
     end
   end
+
+  def email_methods(m)
+    m.constantize.instance_methods(false).map(&:to_s).sort
+  end
+
+  def split_in_halves(elements, &weight)
+    tot_w = elements.map(&weight).sum
+    cols = [elements.dup, []]
+    col2_w = 0
+    cols[0].reverse_each do |cl|
+      n = weight.call(cl)
+      break if col2_w + n > tot_w / 2
+      col2_w += n
+      cols[1] << cols[0].pop
+    end
+    cols[1].reverse!
+    cols
+  end
 end
