35f3d0d3c33d262dd7c81acfd46f962cc9be8106
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-08-08 07:11:13 +0200

    update ui

--- app/helpers/rails_email_preview/emails_helper.rb
+++ app/helpers/rails_email_preview/emails_helper.rb
@@ -1,4 +1,43 @@
 module RailsEmailPreview::EmailsHelper
+  def current_email_name
+    "#{preview_class_human_name}: #{@mail_action.to_s.humanize}"
+  end
+
+  def preview_class_human_name(preview_class = @preview_class)
+    preview_class.to_s.underscore.sub(/(_mailer)?_preview$/, '').humanize
+  end
+
+  def change_locale_attr(locale)
+    {href:  rails_email_preview.rep_email_url(params.merge(part_type: @part_type, email_locale: locale)),
+     class: ('active' if @email_locale == locale.to_s)}
+  end
+
+  def change_format_attr(mime)
+    {href: rails_email_preview.rep_email_url(params.merge(part_type: mime)),
+     class: ('active' if @part_type == mime)}
+  end
+
+  def locale_name(locale)
+    if defined?(TwitterCldr)
+      TwitterCldr::Shared::LanguageCodes.to_language(locale.to_s, :bcp_47)
+    else
+      locale.to_s
+    end
+  end
+
+  def headers_name_value
+    I18n.with_locale @email_locale do
+      {
+          'Subject'  => @mail.subject || '(no subject)',
+          'From'     => @mail.from,
+          'Reply to' => @mail.reply_to,
+          'To'       => @mail.to,
+          'CC'       => @mail.cc,
+          'BCC'      => @mail.bcc
+      }.delete_if { |k, v| v.blank? }
+    end
+  end
+
   def format_header(value)
     if value.is_a?(Array)
       value.map(&:to_s) * ', '
@@ -12,8 +51,8 @@ module RailsEmailPreview::EmailsHelper
   end
 
   def split_in_halves(elements, &weight)
-    tot_w = elements.map(&weight).sum
-    cols = [elements.dup, []]
+    tot_w  = elements.map(&weight).sum
+    cols   = [elements.dup, []]
     col2_w = 0
     cols[0].reverse_each do |cl|
       n = weight.call(cl)
