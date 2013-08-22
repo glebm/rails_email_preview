12a86d0809e57f8758c9d0594db7e78023de9507
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-08-23 01:05:14 +0200

    travis

---
+++ spec/features/take_screenshots_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+unless ENV['TRAVIS']
+  describe 'Take screenshots', driver: :poltergeist do
+    it 'list page' do
+      visit rails_email_preview.rep_root_path
+      screenshot! 'list'
+    end
+
+    it 'show email page' do
+      visit rails_email_preview.rep_email_path('auth_mailer_preview', 'email_confirmation')
+      screenshot! 'show'
+    end
+  end
+end
