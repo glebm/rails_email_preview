f74902e8d657fa426addb6b0300715ddb3c6ea9b
Gleb Mazovetskiy <glex.spb@gmail.com>
2013-09-26 13:32:57 +0200

    refs #21 fix specs

--- spec/features/emails_list_spec.rb
+++ spec/features/emails_list_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe 'emails list' do
   it 'shows emails' do
     visit rails_email_preview.rep_root_path
-    [I18n.t('rep.list_title'), 'Auth', 'Email confirmation', 'Newsletter', 'Weekly newsletter', '4 emails in 2 mailers'].each do |text|
+    [I18n.t('rails_email_preview.emails.index.list_title'), 'Auth', 'Email confirmation', 'Newsletter', 'Weekly newsletter', '4 emails in 2 mailers'].each do |text|
       expect(page).to have_content text
     end
   end
 
   it 'uses REP template by default' do
     visit rails_email_preview.rep_root_path
-    expect(page).to have_title I18n.t('rep.head_title')
+    expect(page).to have_title I18n.t('layouts.rails_email_preview.application.head_title')
   end
 
   it 'uses app template when specified' do
