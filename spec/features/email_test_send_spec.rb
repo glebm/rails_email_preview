cea85de507f6ff796fbe1f5db34a0765454da0d5
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-02-22 02:51:40 +0100

    acceptance test for email test send. fix param name

---
+++ spec/features/email_test_send_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'email test send' do
+  let(:url_args) { {preview_id: 'auth_mailer_preview-email_confirmation'} }
+  it 'shows email' do
+    page.driver.post rails_email_preview.rep_test_deliver_path(url_args), {recipient_email: 'test@test.com'}
+    expect(page.driver.response.location).to eq rails_email_preview.rep_email_url(url_args)
+  end
+end
