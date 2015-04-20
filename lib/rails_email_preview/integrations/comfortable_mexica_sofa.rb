a22749821be0a256d0db6cf040ad586ee67afeca
itschn <lofd23@yahoo.de>
2015-04-20 10:53:56 +0200

    Adjusting emails edit link

--- lib/rails_email_preview/integrations/comfortable_mexica_sofa.rb
+++ lib/rails_email_preview/integrations/comfortable_mexica_sofa.rb
@@ -100,7 +100,8 @@ module RailsEmailPreview
       end
 
       def cms_edit_email_snippet_link(path)
-        link_to(RailsEmailPreview.edit_link_text, path, style: RailsEmailPreview.edit_link_style.html_safe, class: 'btn btn-default pull-right') do
+        link_to path, style: RailsEmailPreview.edit_link_style.html_safe, class: 'btn btn-default pull-right' do
+          RailsEmailPreview.edit_link_text
           content_tag :i, class: 'glyphicon glyphicon-pencil'
         end
       end
